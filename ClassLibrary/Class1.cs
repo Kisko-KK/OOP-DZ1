c5c0371f7f987f58b9d760eb4c4376fa544bdc83
Kisko <marin@gmail.com>
2021-11-03 14:33:46 +0100

    Initial Commit

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Weather
+    {
+        private double temperature;
+        private double humidity;
+        private double windSpeed;
+
+
+
+        //KONSTRUKTORI
+
+        public Weather()
+        {
+            temperature = 0;
+            humidity = 0;
+            windSpeed = 0;
+        }
+
+        public Weather(double temperature, double humidity, double windSpeed)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.windSpeed = windSpeed;
+        }
+
+
+        //SET
+        public void SetTemperature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+        public void SetHumidity(double humidity)
+        {
+            this.humidity = humidity;
+        }
+        public void SetWindSpeed(double windspeed)
+        {
+            this.windSpeed = windspeed;
+        }
+
+        //GET
+        public double GetTemperature()
+        {
+            return temperature;
+        }
+        public double GetHumidity()
+        {
+            return humidity;
+        }
+        public double GetWindSpeed()
+        {
+            return windSpeed;
+        }
+
+        //METODE
+        public double CalculateFeelsLikeTemperature()
+        {
+            return -8.78469475556 + 1.61139411 * temperature + 2.33854883889 * humidity * 100 + -0.14611605 * humidity * 100 * temperature + -0.012308094 * (temperature * temperature) + -0.0164248277778 * ((humidity * 100) * (100 * humidity)) + 0.002211732 * (temperature * temperature) * humidity * 100 + 0.00072546 * temperature * ((humidity * 100) * (100 * humidity)) + -0.000003582 * ((humidity * 100) * (humidity * 100)) * (temperature * temperature);
+        }
+
+        public double CalculateWindChill()
+        {
+            if (temperature <= 10 && windSpeed > 4.8)
+            {
+                return 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(windSpeed, 0.16) + 0.3965 * temperature * Math.Pow(windSpeed, 0.16);
+            }
+            else
+                return 0.0;
+        }
+
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ClassLibrary;
+
+namespace UI
+{
+    class Program
+    {
+
+        static Weather FindWeatherWithLargestWindchill(Weather[] weathers)
+        {
+            int i;
+            double max = -100;
+            Weather maxWeather = weathers[0];
+            for (i = 0; i < weathers.Length; i++)
+            {
+                if (weathers[i].GetTemperature() <= 10 && weathers[i].GetWindSpeed() > 4.8)
+                {
+                    if (weathers[i].CalculateWindChill() > maxWeather.CalculateWindChill())
+                    {
+                        max = weathers[i].CalculateWindChill();
+                        maxWeather = weathers[i];
+                    }
+
+                }
+            }
+
+            return maxWeather;
+        }
+
+
+
+
+        private static void Main(string[] args)
+        {
+            Weather current = new Weather();
+            current.SetTemperature(24.12);
+            current.SetWindSpeed(3.5);
+            current.SetHumidity(0.56);
+            Console.WriteLine("Weather info:\n"
+                + "\ttemperature: " + current.GetTemperature() + "\n"
+                + "\thumidity: " + current.GetHumidity() + "\n"
+                + "\twind speed: " + current.GetWindSpeed() + "");
+            Console.WriteLine("Feels like: " + current.CalculateFeelsLikeTemperature()+"\n");
+
+
+            
+            
+
+            
+            Console.WriteLine("Finding weather info with largest windchill!");
+            const int Count = 5;
+            double[] temperatures = new double[Count] { 8.33, -1.45, 5.00, 12.37, 7.67 };
+            double[] humidities = new double[Count] { 0.3790, 0.4555, 0.743, 0.3750, 0.6612 };
+            double[] windSpeeds = new double[Count] { 4.81, 1.5, 5.7, 4.9, 1.2 };
+
+            Weather[] weathers = new Weather[Count];
+            for (int i = 0; i < weathers.Length; i++)
+            {
+                weathers[i] = new Weather(temperatures[i], humidities[i], windSpeeds[i]);
+                Console.WriteLine("Windchill for weathers[" + i + "] is: " + weathers[i].CalculateWindChill());
+            }
+            Weather largestWindchill = FindWeatherWithLargestWindchill(weathers);
+            Console.WriteLine(
+                "Weather info:" + largestWindchill.GetTemperature() + ", " +
+                largestWindchill.GetHumidity() + ", " + largestWindchill.GetWindSpeed()
+            );
+            
+
+
+
+        }
+    }
+}
